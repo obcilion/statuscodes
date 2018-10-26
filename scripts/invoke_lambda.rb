afab011434a0b318804ac378649166146be80b40
Raymond Gulbrandsen <raymond.gulbrandsen@dnb.no>
2018-10-26 11:07:31 +0200

    :wrench: Invoke script now uses config yaml

--- scripts/invoke_lambda.rb
+++ scripts/invoke_lambda.rb
@@ -1,5 +1,10 @@
-command = 'aws lambda invoke --function-name rayrays_statuslambda ./response.json --profile dnb-core-app-sandbox'
+require 'yaml'
+
+file = File.new(File.join(File.dirname(__FILE__),'deploy_config.yaml'),'r')
+config = YAML.load file.read
+
+command = "aws lambda invoke --function-name #{config["function_name"]} /tmp/response.json --profile #{config["profile_name"]}"
 
 system command
 puts "-----Response JSON-----\n"
-system 'cat ./response.json'
+system 'cat /tmp/response.json'
